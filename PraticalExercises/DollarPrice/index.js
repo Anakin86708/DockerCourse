04a2830ee7b5b6f950ab82dfb7d7aa8e94598162
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2019-10-11 13:51:38 -0300

    Agora são coletadas informações como Rublo, Real e Dólar com uso da API

--- PraticalExercises/DollarPrice/index.js
+++ PraticalExercises/DollarPrice/index.js
@@ -1,9 +1,18 @@
+function convert(money1, money2) {
+  return (1/money1)*money2
+}
 
-
-fetch('https://api.exchangeratesapi.io/latest?symbols=USD,BRL')
+fetch('https://api.exchangeratesapi.io/latest?symbols=RUB,BRL,USD')
   .then(response => response.json())
   .then(json => {
-    console.log(json)
-    document.querySelector("#real").textContent = json.rates.BRL
-document.querySelector("#dolar").textContent = json.rates.USD
+
+    var BRL = json.rates.BRL;
+    var USD = json.rates.USD;
+    var RUB = json.rates.RUB;
+
+
+    document.getElementById("rublo").innerHTML = RUB.toFixed(3);
+    document.getElementById("real_rublo").innerHTML = convert(RUB,BRL).toFixed(3);
+    document.getElementById("dolar").innerHTML = USD.toFixed(3);
+    document.getElementById("real_dolar").innerHTML = convert(USD,BRL).toFixed(3);
   })
